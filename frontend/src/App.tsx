a617974272072d5dfd9698f43fe55f357c4d24c2
sanketpawar1103 <pawarss1103@gmail.com>
2026-04-22 11:26:03 +0530

    Adds the same dummy post on every submit.
    - Mokes the post.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 const FormTitle = () => <h1>Create Post</h1>;
 
 const FormData = () => (
-  <div>
+  <>
     <p>Title</p>
     <input type="text" placeholder="Enter a title..." name="title" />
     <p>Body</p>
@@ -15,31 +15,50 @@ const FormData = () => (
     >
     </textarea>
     <button type="submit">Post</button>
-  </div>
+  </>
 );
 
-const DisplayForm = () => (
-  <form>
+const DisplayForm = ({ saveThePost, posts }) => (
+  <form
+    onSubmit={(e) => {
+      e.preventDefault();
+      saveThePost([...posts, { name: "sanket", body: "StepIntern" }]);
+    }}
+  >
     <FormTitle />
     <FormData />
   </form>
 );
 
-// const DisplayForm = () => {
-//   return (
-//     <div>
-//       <h1>Create Post</h1>
-//       <CreatePostForm />
-//     </div>
-//   );
-// };
+const Feed = ({ post }: { name: string; body: string }) => {
+  return (
+    <>
+      <h2>{post.name}</h2>
+      <p>{post.body}</p>
+    </>
+  );
+};
+
+const CreateFeed = ({ posts }) =>
+  posts.map((p, i) => <Feed post={p} key={i} />);
+
+const DisplayFeed = ({ posts }) => {
+  return (
+    <div>
+      <h1>Feed</h1>
+      <CreateFeed posts={posts} />
+    </div>
+  );
+};
 
 const App = () => {
-  const [count, setCount] = useState(0);
+  const dummyPosts = [{ name: "sanket", body: "Step Intern" }];
+  const [posts, setPosts] = useState(dummyPosts);
 
   return (
     <div>
-      <DisplayForm />
+      <DisplayForm saveThePost={setPosts} posts={posts} />
+      <DisplayFeed posts={posts} />
     </div>
   );
 };
